121a748a088709a1e76b9f7ae4334033b36d7c5b
Carlos Queiroz <cqueiroz@gopivotal.com>
2015-06-24 08:42:24 -0400

    adds spaces between commas. to format.

--- recv.py
+++ recv.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python
 import pika
 credentials = pika.PlainCredentials('guest', 'guest')
-connection = pika.BlockingConnection(pika.ConnectionParameters(host='localhost',credentials=credentials))
+connection = pika.BlockingConnection(pika.ConnectionParameters(host='localhost', credentials=credentials))
 channel = connection.channel()
 print ' [*] Waiting for messages. To exit press CTRL+C'
 def callback(ch, method, properties, body):
     print " [x] Received %r" % (body,)
-channel.basic_consume(callback,queue='FILE_PROC_QUEUE',no_ack=True)
+channel.basic_consume(callback, queue='FILE_PROC_QUEUE', no_ack=True)
 channel.start_consuming()
 
